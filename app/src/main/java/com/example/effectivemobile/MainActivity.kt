c4b324cbb031badcc10f8e5171929d00170f4851
BIBLETUM <pasha.remizov@mail.ru>
2024-07-05 15:25:39 +0300

    modal_window implemented

--- app/src/main/java/com/example/effectivemobile/MainActivity.kt
+++ app/src/main/java/com/example/effectivemobile/MainActivity.kt
@@ -5,10 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.effectivemobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragment = MainFragment.newInstance()
+        supportFragmentManager.beginTransaction().replace(binding.mainContainer.id, fragment).commit()
+
+        val modalBottomSheet = BottomSheetSearch()
+        modalBottomSheet.show(supportFragmentManager, BottomSheetSearch.TAG)
     }
 }
--- app/src/main/java/com/example/effectivemobile/MainFragment.kt
+++ app/src/main/java/com/example/effectivemobile/MainFragment.kt
@@ -31,12 +31,8 @@ class MainFragment : Fragment() {
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            MainFragment()
+
     }
 }
