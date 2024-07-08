ad219261398634f696fef5b7570544692113894c
BIBLETUM <pasha.remizov@mail.ru>
2024-07-09 01:10:51 +0300

    added SearchFeaturedTicketsFragment

--- app/src/main/java/com/example/effectivemobile/MainActivity.kt
+++ app/src/main/java/com/example/effectivemobile/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val fragment = MainFragment.newInstance()
+        val fragment = SearchFeaturedTicketsFragment.newInstance()
         supportFragmentManager.beginTransaction().replace(binding.mainContainer.id, fragment).commit()
 
         val modalBottomSheet = BottomSheetSearch()
